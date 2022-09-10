167185e0f2a72edebef5d6543644bbcafc6f34dc
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-09-10 02:30:26 -0400

    linked in hook practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useTrees} from "./index"
 
 function App() {
+  
+  const { trees } = useTrees
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Trees I've Heard of</h1>
+      <ul>
+        {trees.map(tree => (
+          <li key={tree.id}>{tree.type}</li>
+        ))}
+      </ul>
     </div>
-  );
+ )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useFetch } from "./useFetch";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function App({ login }) {
+  const { loading, data, error } = useFetch(
+      `https://api.github.com/users?/${login}`
+    )
+
+    console.log(data)
+
+  if(loading) return <h1>loading...</h1>
+  if(error) 
+    return (
+      <pre>{JSON.stringify(error, null, 2)}</pre>
+    )
+    
+    return (
+      <>
+      <div>
+      <ul>
+        {data.map((eachData) => {
+          return(
+            <div>
+                <h1>{eachData.login}</h1>
+                <li>{eachData.type}</li>
+                <li>{eachData.url}</li>
+                <p>                  
+                <img src={eachData.avatar_url} alt="" height='160' width='200'/>
+                </p>
+            </div>
+        )})}
+      </ul>
+    </div>
+    </>
+  
+  )
+
+
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
